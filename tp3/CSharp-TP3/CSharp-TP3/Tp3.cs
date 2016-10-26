a8ada9e1872dbb2f0ab7d25376e3e42925c4f72b
Bérénice Faltrept <berenice.faltrept@gmail.com>
2016-10-26 13:49:32 +0200

    tp3 -last version

---
+++ tp3/CSharp-TP3/CSharp-TP3/Tp3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpTP3{
+	
+	public static class MainClass
+	{
+		public static List<T> Map<T2, T>(List<T2> l, Func<T2, T> fgen){
+			List<T> l_res = new List<T>(l.Count);
+			foreach (T2 tval in l) {
+				l_res.Add(fgen(tval));
+			}
+			return l_res;
+		}
+
+		public static Dictionary<T,List<T2>> Convert<T, T2>(List<KeyValuePair<T,T2>> l){
+			Dictionary<T,List<T2>> dico = new Dictionary<T, List<T2>>();
+			foreach (KeyValuePair<T,T2> lval in l) {
+				if (!dico.ContainsKey (lval.Key)) {
+					List<T2> val = new List<T2> ();
+					val.Add (lval.Value);
+					dico.Add (lval.Key, val);
+				} else {
+					List<T2> tmp = dico [lval.Key];
+					tmp.Add(lval.Value);
+				}
+			}
+			return dico;
+		}
+
+		public static void Main ()
+		{
+
+			List<KeyValuePair <int, string>> list = new List<KeyValuePair <int, string>> ();
+
+			list.Add (new KeyValuePair<int, string> (1, "un"));
+			list.Add (new KeyValuePair<int, string> (7, "sept"));
+			list.Add (new KeyValuePair<int, string> (1, "uno"));
+
+			List<int> keys = Map (list, i => i.Key);
+			foreach (int k in keys)
+				Console.WriteLine ("{0} : {1}", k, list.Find (i => (i.Key == k)).Value);
+			
+			Console.WriteLine ("");Console.WriteLine (" -- ");Console.WriteLine ("");
+
+			Dictionary<int, List<string>> dic = Convert (list);
+			foreach (KeyValuePair<int, List<string>> entry in dic) {
+				Console.WriteLine ("{0}",entry.Key);
+				foreach(string elmt in entry.Value)
+					Console.WriteLine ("    {0}",elmt);
+			}
+		}
+
+	}
+}
+/*
+Différence entre programmation générique C# et Ocaml : 
+	En C#, il faut préciser énormément d'informations sur le générique, 
+	là ou le Caml a une inférence de type et la curryfication qui allègent le code.
+*/
--- tp3/tp3.ml
+++ tp3/tp3.ml
@@ -3,12 +3,15 @@
 (* Q1 *)
 let f1 (a, b, c) = b;;
 
-(* Q2 *)
+    (* Q2 *)
 let testc x y = x+y;;
 let testdec (x, y) = x+y;;
 
 let curry f x y = f (x,y);;
 let decurry f (x,y) = f x y;;
+
+let curry_fun f = fun x y -> f (x, y);;
+let decurry_fun f = fun (x,y) -> f x y;;
 
 let testc_decur = decurry testc;;
 let testdec_cur = curry testdec;;
@@ -16,30 +19,20 @@ let testdec_cur = curry testdec;;
 testc_decur (4,5);;
 testdec_cur 4 5;;
 
-(* Q3 *)
+    (* Q3 *)
 
 let inverse f x y = f y x ;;                          
 
-(* Q4 *)
+    (* Q4 *)
 
-let test x = x*2;;
+let example x = x*2;;
 
-(* REFAIRE - pas assez générique*)
-let rec iterate f n = if n = 0
-  then f
-  else f f(n-1));;
+let rec iterate f n x =
+  match n with
+  | 0 -> f x
+  | n -> f (iterate f (n-1) x);;
 
-let rec iterate f n = match n with
-  | 0 -> f
-  | n -> (f (iterate f (n-1)));;
-
-(*
-Error: This expression has type 'a -> 'b
-       but an expression was expected of type 'a
-       The type variable 'a occurs inside 'a -> 'b
-*)
-
-(* Ex2 -liste *)
+    (* Ex2 -liste *)
 
 (*
   'a list => liste de type générique.
@@ -47,10 +40,9 @@ Error: This expression has type 'a -> 'b
  - : int list = [1]
 
   [ (1,"true")  ; (2,"false") ] => liste de tuples (tuples du MEME format)
-
 *)
 
-let list_int = 4::5::7::[];;
+et list_int = 4::5::7::[];;
 let rec voir l =
   match l with
     | [] -> print_string " "
@@ -73,20 +65,80 @@ let rec list_map f l =
 
 let list_int2 = 6::2::8::[];;
 let list2 = [(7,"vb");(1,"dc");(5,"ws");(3,"jy")];;
+
 (* version non recursive : let list_concat_term l1 l2 = l1@l2;; *)
 
-let rec list_concat_term l1 l2 =
+let rec list_concat_term l1 l2 = 
   match l2 with
     | [] -> l1; (*rec terminal*)
-    | t::q -> list_concat_term t::l1 q;;
+    | t::q -> list_concat_term (t::l1) q;;
 
 
-(* Ex3 -Objet Enregistrements *)
+
+
+    (* Ex3 -Objet Enregistrements *)
 
 (* Q1 *)
+
+(*version non objet : let addC c1 c2 = {a = c1.a +. c2.a ; b=c1.b +. c2.b};;*)
+
+(* I -  version incomplète - mais fonctionnelle *)
+
+type complexeV1 = {
+  a : float;
+  b : float;
+}
+
+let constructeur a1 b1 = {a = a1 ; b=b1};;
+
+let c1 = constructeur 5. 7.;;
+let c2 = {a=2.;b=31.};;
+
+(* II - ajout de fonction ressemblant à de l'objet - ADD *)
+
 type complexe = {
   a : float;
   b : float;
-  add : 'a 'a -> 'a;
+  add : complexe -> complexe;
 };;
+
+let e1 = {a=5.;
+	  b=7.;
+	  add = fun c2 -> { a = e1.a +. c2.a ; b = e1.b +. c2.b ; add = c2.add ;}};;
+
+
+let e2 = e1.add e1;;
+
+
+
+(* III - ajout de fonction ressemblant à de l'objet - MULT  --> tentative *)
+
+type complexe = {
+  a : float;
+  b : float;
+  add : complexe -> complexe;
+  mult : complexe -> complexe;
+};;
+
+let e1 = {a=5.;
+	  b=7.;
+	  add = fun c2 -> { a = e1.a +. c2.a ;
+			    b = e1.b +. c2.b ;
+			    add = c2.add ;
+			    mult= c2.mult} ;
+	    mult = fun c2 -> { a = e1.a *. c2.a ; (* Se croirait-il encore dans add ? Pourquoi une telle indentation ? Comment corriger cela ? *)
+			    b = e1.b *. c2.b ;
+			    add = c2.add ;
+			    mult= c2.mult}
+	 };;
+
+(*
+erreur :
+This expression has type complexe/1303 -> complexe/1303
+       but an expression was expected of type complexe/1325 -> complexe/1325
+       Type complexe/1303 is not compatible with type complexe/1325 
+*)
+
+
+
 
